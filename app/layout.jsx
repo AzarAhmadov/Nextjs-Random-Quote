ebcc87c5ad3f6c0d762e92ed78b95f59088639f3
Azar Ahmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-03-10 03:10:36 -0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <title> Quote App </title>
       <body>{children}</body>
     </html>
   )
